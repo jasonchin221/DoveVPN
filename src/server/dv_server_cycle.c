ba1756631a241fbe5b8717d0080abb9c91abf81a
jason <jasonchin221@163.com>
2016-10-17 22:22:06 +0800

    save
    Signed-off-by: jason <jasonchin221@163.com>

--- src/server/dv_server_cycle.c
+++ src/server/dv_server_cycle.c
@@ -12,9 +12,42 @@ static dv_tun_t dv_srv_tun = {
 };
 
 static int
-dv_start_worker_processes(dv_srv_conf_t *conf, dv_u32 cpu_num)
+dv_srv_create_and_set_tun(dv_tun_t *tun, int seq, int mask,
+            char *subnet_ip, dv_u32 subet_ip_size)
 {
     dv_subnet_ip_t  *ip = NULL;
+    int             ret = DV_ERROR;
+
+    ret = dv_tun_dev_create(tun, seq);
+    if (ret != DV_OK) {
+        return DV_ERROR;
+    }
+
+    ret = dv_ip_pool_init(subnet_ip, subnet_ip_size, mask);
+    if (ret != DV_OK) {
+        goto err;
+    }
+
+    ip = dv_subnet_ip_alloc();
+    if (ip == NULL) {
+        goto err;
+    }
+
+    /* Config ip for tun */
+    ret = dv_if_set_ip(tun->tn_name, ip->si_ip, mask);
+    if (ret != DV_OK) {
+        goto err;
+    }
+
+    return DV_OK;
+err:
+    dv_tun_dev_destroy(tun);
+    return ret;
+}
+
+static int
+dv_start_worker_processes(dv_srv_conf_t *conf, dv_u32 cpu_num)
+{
     dv_tun_t        *tun = &dv_srv_tun;
     int             mask = conf->sc_subnet_mask;
     int             i = 0;
@@ -23,30 +56,13 @@ dv_start_worker_processes(dv_srv_conf_t *conf, dv_u32 cpu_num)
     for (i = 0; i < 1/* cpu_num */; i++) {
         /* Fork process */
 
-        ret = dv_tun_dev_create(tun, i);
+        ret = dv_srv_create_and_set_tun(tun, i, mask,
+            conf->sc_subnet_ip, sizeof(conf->sc_subnet_ip));
         if (ret != DV_OK) {
             break;
         }
 
-        ret = dv_ip_pool_init(conf->sc_subnet_ip, sizeof(conf->sc_subnet_ip),
-                mask);
-        if (ret != DV_OK) {
-            break;
-        }
-        
-        ip = dv_subnet_ip_alloc();
-        if (ip == NULL) {
-            ret = DV_ERROR;
-            break;
-        }
-        
-        /* Config ip for tun */
-        ret = dv_if_set_ip(tun->tn_name, ip->si_ip, mask);
-        if (ret != DV_OK) {
-            break;
-        }
-        
-        /* Libevent */
+       /* Add event */
     }
 
     return ret;
@@ -62,11 +78,14 @@ dv_server_cycle(dv_srv_conf_t *conf)
         goto out;
     }
 
+    /* Libevent */
+
     ret = dv_start_worker_processes(conf, dv_ncpu);
     if (ret != DV_OK) {
         goto out;
     }
 
+    /* Event loop */
     sleep(100);
     ret = DV_OK;
 out:
