2e6d6e8267209b5c4112f1912c853c138f3eaf0c
jason <jasonchin221@163.com>
2016-10-21 22:12:46 +0800

    save
    Signed-off-by: jason <jasonchin221@163.com>

--- src/server/dv_server_socket.c
+++ src/server/dv_server_socket.c
@@ -11,6 +11,9 @@
 #define DV_SERVER_LISTEN_NUM    100
 #define DV_SERVER_BUF_SIZE      16384
 
+static void
+dv_srv_write_handshake(int sock, short event, void *arg);
+
 dv_sk_conn_t *
 dv_sk_conn_alloc(size_t buf_size)
 {
@@ -85,8 +88,22 @@ dv_srv_read(int sock, short event, void *arg)
     dv_event_destroy(ev);
 }
 
+static int
+dv_srv_handshake_done(dv_event_t *ev, const dv_proto_suite_t *suite)
+{
+    dv_sk_conn_t        *conn = ev->et_conn;
+    void                *ssl = conn->sc_ssl;
+
+    if (suite->ps_get_verify_result(ssl) != DV_OK) {
+        return DV_ERROR;
+    }
+    ev->et_handler = dv_srv_read;
+
+    return DV_OK;
+}
+
 static void
-dv_srv_handshake(int sock, short event, void *arg)
+dv_srv_read_handshake(int sock, short event, void *arg)
 {
     const dv_proto_suite_t  *suite = dv_srv_proto_suite;
     dv_event_t              *ev = arg; 
@@ -101,12 +118,12 @@ dv_srv_handshake(int sock, short event, void *arg)
     /* 建立 SSL 连接 */
     ret = suite->ps_accept(ssl);
     if (ret == DV_OK) {
-        if (suite->ps_get_verify_result(ssl) != DV_OK) {
+        ret = dv_srv_handshake_done(ev, suite);
+        if (ret != DV_OK) {
             printf("Client cert verify failed!\n");
             goto out;
         }
-        //handler = dv_srv_read;
-        conn->sc_flags |= DV_SK_CONN_FLAG_HANDSHAKED;
+        //conn->sc_flags |= DV_SK_CONN_FLAG_HANDSHAKED;
         return;
     }
 
@@ -115,14 +132,69 @@ dv_srv_handshake(int sock, short event, void *arg)
     }
 
     if (ret == DV_EWANT_WRITE) {
-            //add write event
+        dv_event_del(ev);
+        ev->et_handler = dv_srv_write_handshake;
+        dv_event_set_write(sock, ev);
+        if (dv_event_add(ev) != DV_OK) {
+            goto out;
+        }
         return;
     }
 
 out:
+    close(sock);
     dv_event_del(ev);
     dv_event_destroy(ev);
 }
+
+static void
+dv_srv_write_handshake(int sock, short event, void *arg)
+{
+    const dv_proto_suite_t  *suite = dv_srv_proto_suite;
+    dv_event_t              *ev = arg; 
+    dv_sk_conn_t            *conn = NULL;
+    void                    *ssl = NULL;
+    int                     ret = DV_OK;
+
+    printf("Handshake!\n");
+    ssl = conn->sc_ssl;
+    conn = ev->et_conn;
+
+    /* 建立 SSL 连接 */
+    ret = suite->ps_accept(ssl);
+    if (ret == DV_OK) {
+        ret = dv_srv_handshake_done(ev, suite);
+        if (ret != DV_OK) {
+            printf("Client cert verify failed!\n");
+            goto out;
+        }
+        goto want_read;
+    }
+
+    if (ret == DV_EWANT_READ) {
+        ev->et_handler = dv_srv_read_handshake;
+want_read:
+        dv_event_set_read(sock, ev);
+        if (dv_event_add(ev) != DV_OK) {
+            goto out;
+        }
+
+        return;
+    }
+
+    if (ret == DV_EWANT_WRITE) {
+        if (dv_event_add(ev) != DV_OK) {
+            goto out;
+        }
+        return;
+    }
+
+out:
+    close(sock);
+    dv_event_del(ev);
+    dv_event_destroy(ev);
+}
+
 
 static void
 _dv_srv_accept(int sock, short event, void *arg, struct sockaddr *addr,
@@ -154,37 +226,37 @@ _dv_srv_accept(int sock, short event, void *arg, struct sockaddr *addr,
     }
     /* 将连接用户的 socket 加入到 SSL */
     suite->ps_set_fd(ssl, accept_fd);
-    /* 建立 SSL 连接 */
-    ret = suite->ps_accept(ssl);
-    switch (ret) {
-        case DV_OK:
-            if (suite->ps_get_verify_result(ssl) != DV_OK) {
-                printf("Client cert verify failed!\n");
-                goto out;
-            }
-            handler = dv_srv_read;
-            break;
-        case DV_EWANT_READ:
-            handler = dv_srv_handshake;
-            break;
-        case DV_EWANT_WRITE:
-            //add write event
-            handler = dv_srv_handshake;
-            break;
-        default:
-            goto out;
-    }
-
     ev = dv_event_create();
     if (ev == NULL) {
         goto out;
     }
 
+    /* 建立 SSL 连接 */
+    ret = suite->ps_accept(ssl);
+    switch (ret) {
+        case DV_OK:
+            ret = dv_srv_handshake_done(ev, suite);
+            if (ret != DV_OK) {
+                printf("Client cert verify failed!\n");
+                goto out;
+            }
+            dv_event_set_read(accept_fd, ev);
+            break;
+        case DV_EWANT_READ:
+            ev->et_handler = dv_srv_read_handshake;
+            dv_event_set_read(accept_fd, ev);
+            break;
+        case DV_EWANT_WRITE:
+            ev->et_handler = dv_srv_write_handshake;
+            dv_event_set_write(accept_fd, ev);
+            break;
+        default:
+            goto out;
+    }
+
     conn->sc_ssl = ssl;
     ev->et_conn = conn;
     ev->et_conn_free = dv_sk_conn_free;
-    ev->et_handler = handler;
-    dv_event_set_read(accept_fd, ev);
     if (dv_event_add(ev) != DV_OK) {
         close(accept_fd);
         dv_event_destroy(ev);
@@ -192,6 +264,10 @@ _dv_srv_accept(int sock, short event, void *arg, struct sockaddr *addr,
     }
     return;
 out:
+    if (ev != NULL) {
+        dv_event_destroy(ev);
+    }
+
     if (conn != NULL) {
         dv_sk_conn_free(conn);
     }
