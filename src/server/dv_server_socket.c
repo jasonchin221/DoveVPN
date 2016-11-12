80bb732f4a53aa67de142efa061c9aa2698c0a69
jason <jasonchin221@n163.com>
2016-11-11 23:18:44 -0500

    save
    Signed-off-by: jason <jasonchin221@n163.com>

--- src/server/dv_server_socket.c
+++ src/server/dv_server_socket.c
@@ -217,7 +217,7 @@ dv_srv_ssl_handshake_done(int sock, dv_event_t *ev, const dv_proto_suite_t *suit
     conn->sc_ip = ip;
 
     mlen = dv_msg_ipalloc_build(wbuf->bf_head, wbuf->bf_bsize,
-            ip->si_ip, strlen(ip->si_ip), dv_get_subnet_mask(),
+            ip->si_ip, strlen(ip->si_ip) + 1, dv_get_subnet_mask(),
             dv_get_subnet_mtu(), dv_route_net, dv_route_mask);
     if (mlen == 0) {
         DV_LOG(DV_LOG_INFO, "Build ipalloc msg failed!\n");
