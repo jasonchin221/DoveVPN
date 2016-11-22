6408a9b3cf5a3e7564a272a329fe68619973543c
jason <jasonchin221@n163.com>
2016-11-22 04:20:46 -0500

    save
    Signed-off-by: jason <jasonchin221@n163.com>

--- src/server/dv_server_socket.c
+++ src/server/dv_server_socket.c
@@ -77,7 +77,6 @@ dv_srv_ssl_to_tun(int sock, short event, void *arg)
     dv_buffer_t             *rbuf = &conn->sc_rbuf;
     int                     tun_fd = dv_srv_tun.tn_fd;
 
-    DV_LOG(DV_LOG_INFO, "SSL data in!\n");
     dv_ssl_read_handler(sock, event, arg, ssl, tun_fd, suite, rbuf,
             dv_get_subnet_mtu(), dv_srv_ssl_err_handler);
 }
